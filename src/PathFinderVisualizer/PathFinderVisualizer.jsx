2edd5540f48a3b434ee0b985ecda387cef11485d
elvisyong <elvisyong@gmail.com>
2021-10-09 20:05:16 +0800

    Cleanup unused stuff

---
+++ src/PathFinderVisualizer/PathFinderVisualizer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Node from './Node/Node'
+
+const PathFinderVisualizer = (props) => {
+  return (
+    <div>
+      <Node></Node>
+    </div>
+  )
+}
+
+export default PathFinderVisualizer;
